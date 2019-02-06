da759bbd23f8f4b036fdd1b4bd2c185565b24030
fierrito2015 <47386049+fierrito2015@users.noreply.github.com>
2019-02-06 16:44:28 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,12 +4,30 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+    var numero1;
+    var numero2;
+    var suma;
+    numero1 = document.getElementById("numeroUno").value;
+    numero2 = document.getElementById("numeroDos").value;
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    suma = numero1 + numero2;
+    alert ("la suma es "+ suma);
+ 
 }
 
 function restar()
 {
-	
+    var numero3;
+    var numero4;
+    var resta;
+    numero3 = document.getElementById("numeroUno").value;
+    numero4 = document.getElementById("numeroDos").value;
+    numero3 = parseInt(numero3);
+    numero4 = parseInt(numero4);
+    resta = numero3 - numero4;
+    alert ("la resta es " + resta);
+
 }
 
 function multiplicar()
