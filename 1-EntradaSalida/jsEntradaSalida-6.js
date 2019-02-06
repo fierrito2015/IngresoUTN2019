07ccbe9a9676017beede495cf068c80217edb3f8
fierrito2015 <47386049+fierrito2015@users.noreply.github.com>
2019-02-06 14:42:17 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,15 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+    var numero1;
+    var numero2;
+    var suma;
+    numero1 = document.getElementById("numeroUno").Value;
+    numero2 = document.getElementById("numeroDos").Value;
+    numero1 = parseint(numero1);
+    numero2 = parseint(numero2);
+    suma = numero1 + numero2;
+    alert(suma);
 
 }
 
