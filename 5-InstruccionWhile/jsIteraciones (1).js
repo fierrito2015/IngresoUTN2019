8004f1c1cc271bc417a2e759fbe97101db1bb38e
fierrito2015 <47386049+fierrito2015@users.noreply.github.com>
2019-02-20 19:58:15 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,12 @@
 function Mostrar()
 {
-	alert('iteración while');
+	var contador;
+	contador = 1
+	while (contador<11)
+	{
+		alert ("Numero "+contador);
+		contador = contador + 1;
+	}
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,12 @@
 function Mostrar()
 {
-	alert('iteración while');
+	var contador;
+	contador = 10;
+	while (contador>0)
+	{
+		alert ("Numero "+contador);
+		contador = contador - 1;
+	}
 
 
 }//FIN DE LA FUNCIÓN
