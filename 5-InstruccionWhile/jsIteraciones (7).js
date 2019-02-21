84f312f37f3858babc67b2c8225888db5e4d4f3c
fierrito2015 <47386049+fierrito2015@users.noreply.github.com>
2019-02-21 16:20:59 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,10 +3,22 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta="si";
+	var numero;
+	var promedio;
+	
+	while (respuesta == "si")
+	{
+		numero = parseInt (prompt("Ingerese un numero: "));
+		acumulador = acumulador + numero;
+		contador = contador +1 ;
+		respuesta = prompt("Desea seguir ingresando numeros? si o no ?").toLowerCase();
+		
+	}
+	promedio = acumulador / contador ;
 
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+document.getElementById('promedio').value=promedio;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,23 @@ function Mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
+	var numero1;
+	var numero2
 	
-	var respuesta='si';
+	var respuesta="si";
+	while (positivo>=0 && respuesta =="si")
+	{
+		numero1 = parseInt(prompt("Ingrese un numero: "));
+		positivo = positivo + numero1;
+		respuesta = prompt("Desea seguir ingresando numeros?").toLowerCase();
+	}
+	while (negativo<1 && respuesta == "si")
+	{
+		numero2 = parseInt(prompt("Ingrese un numero: "));
+		negativo = negativo * numero2;
+		respuesta = prompt("Desea seguir ingresando numeros?").toLowerCase();
+
+	}
 
 
 document.getElementById('suma').value=positivo;
