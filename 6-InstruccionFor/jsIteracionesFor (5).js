2ceceaa8237f40498448e6a6d4619acd309a73ca
fierrito2015 <47386049+fierrito2015@users.noreply.github.com>
2019-02-28 12:28:42 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,6 +1,21 @@
 function Mostrar()
 {
 
+var numero
+
+for (;;)
+{
+    numero = parseInt(prompt("Ingrese un numero"));
+    while (isNaN(numero))
+    {
+        numero = parseInt(prompt("Error. Ingrese un numero"));
+    }
+    if (numero==9)
+    {
+        break;
+    }
+}
+
 
 
 
--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,6 +1,23 @@
 function Mostrar()
 {
-
+var numero
+var contadorPares=0
+numero= parseInt(prompt("Ingrese un numero POSITIVO"));
+ while (numero<=0||isNaN(numero))
+    {
+        numero = parseInt(prompt("Error. Ingrese un numero POSITIVO"));
+    }
+for (var i=1;i<=numero;i++)
+{
+    
+   
+    if (i%2==0)
+    {
+       contadorPares++
+       console.log(i);
+    }
+}
+console.log("La cantidad de pares es: "+contadorPares);
 
 
 
